ed3d9691c9fa0bb8a94c5775d999fa7c21416152
Aditi Bishnoi <bishnoi.aditi6@gmail.com>
2026-03-10 17:36:27 +0530

    added

---
+++ src/components/Aboutus.js
@@ -0,0 +1,64 @@
+{/* ABOUT SECTION */}
+
+<section className="about-section">
+
+<div className="container about-wrapper">
+
+{/* LEFT CONTENT */}
+
+<div className="about-content" data-aos="fade-right">
+
+<span className="about-tag">ABOUT US</span>
+
+<h2>
+We Build Digital Products
+That Drive Business Growth
+</h2>
+
+<p className="about-desc">
+Softcoders helps businesses transform ideas into powerful
+software solutions. Our team builds modern web applications,
+mobile apps and scalable platforms designed for long-term
+business success.
+</p>
+
+<div className="about-highlights">
+
+<div className="highlight-item" data-aos="fade-up" data-aos-delay="100">
+<FaCheckCircle className="highlight-icon"/>
+<span>Modern Technologies</span>
+</div>
+
+<div className="highlight-item" data-aos="fade-up" data-aos-delay="200">
+<FaPalette className="highlight-icon"/>
+<span>Clean UI / UX Design</span>
+</div>
+
+<div className="highlight-item" data-aos="fade-up" data-aos-delay="300">
+<FaDollarSign className="highlight-icon"/>
+<span>Affordable Development</span>
+</div>
+
+<div className="highlight-item" data-aos="fade-up" data-aos-delay="400">
+<FaHeadset className="highlight-icon"/>
+<span>24/7 Client Support</span>
+</div>
+
+</div>
+
+</div>
+
+
+{/* RIGHT IMAGE */}
+
+<div className="about-image" data-aos="fade-left">
+
+<img src={aboutus} alt="Client Meeting"/>
+
+</div>
+
+</div>
+
+</section>
+
+export default Aboutus;
